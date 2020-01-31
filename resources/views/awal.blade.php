5f08b4e2c65aa294d96118773e67c2cab6ec6241
adeputri123 <44240325+adeputri123@users.noreply.github.com>
2020-01-31 11:54:34 +0700

    First Commit

---
+++ resources/views/awal.blade.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Sign In | Bootstrap Based Admin Template - Material Design</title>
+    <!-- Favicon-->
+    <link rel="icon" href="../../favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="login-page">
+    <div class="login-box">
+        <div class="logo">
+          <img src="{{ ('images/bps.png') }}" width="200px" height="200px" style="display: block; margin: auto; padding-bottom: 25px;">
+            <a href="javascript:void(0);"><b>SISMAK</b></a>
+            <small>Sistem Informasi Surat Masuk dan Surat Keluar</small>
+        </div>
+        <div class="body">
+          <div class="demo-button">
+            <button type="button" class="btn btn-block btn-lg btn-danger waves-effect">Surat Masuk</button>
+            <button type="button" class="btn btn-block btn-lg btn-danger waves-effect">Surat Keluar</button>
+            <button type="button" class="btn btn-block btn-lg btn-danger waves-effect">Disposisi</button>
+            <button type="button" class="btn btn-block btn-lg btn-danger waves-effect">Arsip</button>
+            <button type="button" class="btn btn-block btn-lg btn-danger waves-effect">Login</button>
+          </div>
+        </div>
+      </div>
+
+    <!-- Jquery Core Js -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="../../plugins/node-waves/waves.js"></script>
+
+    <!-- Validation Plugin Js -->
+    <script src="../../plugins/jquery-validation/jquery.validate.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../../js/admin.js"></script>
+    <script src="../../js/pages/examples/sign-in.js"></script>
+</body>
+
+</html>
